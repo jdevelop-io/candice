40554105d9aa739ac406e8c146af529264f73c1f
Jean-Denis VIDOT <5817251+jh3ady@users.noreply.github.com>
2025-03-22 19:24:18 +0100

    :wrench: chore(phpstan): Setup phpstan (#5)
    * :heavy_plus_sign: chore(deps): add phpstan/phpstan dev dependency
    
    * :see_no_evil: chore(git): ignore phpstan.neon file
    
    * :wrench: chore(phpstan): Add default configuration
    
    * :heavy_plus_sign: chore(deps): Add thecodingmachine/safe dependency
    
    * :heavy_plus_sign: chore(deps): Add thecodingmachine/phpstan-safe-rule dev dependency
    
    * :wrench: chore(docker): Add phpstan service
    
    * :wrench: chore(makefile): Add phpstan command
    
    * :truck: chore: Rename phpstan.dist.neon to phpstan.neon.dist
    
    * :construction_worker: ci(conformity): Add phpstan
    
    * :rotating_light: fix(phpstan): Fix public/index.php file

--- public/index.php
+++ public/index.php
@@ -5,5 +5,9 @@ use Candice\Shared\Infrastructure\Symfony\Kernel;
 require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context) {
+    if (!is_string($context['APP_ENV'])) {
+        throw new \RuntimeException('APP_ENV environment variable is not set.');
+    }
+
     return new Kernel($context['APP_ENV'], (bool)$context['APP_DEBUG']);
 };
