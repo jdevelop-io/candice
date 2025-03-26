7be409098f09cc6666d498753ad8ea0dafb99901
Jean-Denis VIDOT <5817251+jh3ady@users.noreply.github.com>
2025-03-26 22:24:26 +0100

    :construction_worker: ci(tests): try to fix phpunit coverage

--- docker/tools/entrypoint.sh
+++ docker/tools/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-## Install dependencies if vendor directory does not exist
-#if [ ! -d "/var/www/html/vendor" ]; then
-#    composer install
-#fi
+# Install dependencies if vendor directory does not exist
+if [ ! -d "/var/www/html/vendor" ]; then
+    composer install
+fi
 
 echo "Running entrypoint.sh"
 echo "Current directory: $(pwd)"
