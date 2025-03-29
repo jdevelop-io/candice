93db2723b81613bb67989fa7d1bf9e711b95590d
Jean-Denis VIDOT <5817251+jh3ady@users.noreply.github.com>
2025-03-29 16:48:40 +0100

    :truck: chore: rename app.css to app.scss to use scss syntax

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,4 @@
-import { registerReactControllerComponents } from '@symfony/ux-react';
+import {registerReactControllerComponents} from '@symfony/ux-react';
 import './bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
@@ -6,8 +6,7 @@ import './bootstrap.js';
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
-
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+// any CSS you import will output into a single css file (app.scss in this case)
+import './styles/app.scss';
 
 registerReactControllerComponents(require.context('./react/controllers', true, /\.(j|t)sx?$/));
