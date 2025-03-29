559df3c8cc2534667fcdf73e035e5e0a50b60d03
Jean-Denis VIDOT <5817251+jh3ady@users.noreply.github.com>
2025-03-29 16:57:10 +0100

    :wrench: chore: Setup typescript

--- assets/app.ts
+++ assets/app.ts
@@ -1,5 +1,5 @@
-import {registerReactControllerComponents} from '@symfony/ux-react';
-import './bootstrap.js';
+import { registerReactControllerComponents } from '@symfony/ux-react'
+import './bootstrap'
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -7,6 +7,6 @@ import './bootstrap.js';
  * (and its CSS file) in your base layout (base.html.twig).
  */
 // any CSS you import will output into a single css file (app.scss in this case)
-import './styles/app.scss';
+import './styles/app.scss'
 
-registerReactControllerComponents(require.context('./react/controllers', true, /\.(j|t)sx?$/));
+registerReactControllerComponents(require.context('./react/controllers', true, /\.([jt])sx?$/));
