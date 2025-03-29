78f14146601f1b90bdf90c8c66be3e985ea9a880
Jean-Denis VIDOT <5817251+jh3ady@users.noreply.github.com>
2025-03-29 19:11:00 +0100

    :lipstick: chore(Homepage): Center block at the center of the screen

--- assets/react/controllers/Hello.tsx
+++ assets/react/controllers/Hello.tsx
@@ -7,9 +7,11 @@ interface HelloProps {
 
 export default function (props: HelloProps) {
     return (
-        <div className="p-4 font-bold text-2xl flex gap-4">
-            <span>Hello { props.fullName }</span>
-            <Button>Let's shine !</Button>
+        <div className="flex flex-col items-center justify-center h-screen">
+            <div className="p-4 border shadow-sm rounded-md font-bold text-2xl inline-flex gap-4">
+                <span>Hello { props.fullName }</span>
+                <Button>Let's shine !</Button>
+            </div>
         </div>
     )
 }
