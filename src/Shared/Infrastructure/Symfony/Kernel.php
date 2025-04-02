5138b167a4f46f01b59e0e1c4e12b7e2971af599
Jean-Denis VIDOT <5817251+jh3ady@users.noreply.github.com>
2025-04-02 23:07:26 +0200

    :wrench: chore(psalm): Setup psalm (#13)
    * :heavy_plus_sign: chore(deps): Add vimeo/psalm dev dependency
    
    * :art: chore(psalm): Resolve problems
    
    * :building_construction: chore(docker): Add psalm service
    
    * :wrench: chore(psalm): Add default configuration
    
    * :wrench: chore(makefile): Add psalm-fix command
    
    Run "make psalm-fix" to run Psalm
    
    * :construction_worker: ci(conformity): Add checks step to run Psalm
    
    * :see_no_evil: chore(git): Ignore the overridden Psalm configuration
    
    * :art: chore(psalm): Resolve problems
    
    * :wrench: chore(psalm): Update default configuration
    
    Check bin, config, and tests folder in addition

--- src/Shared/Infrastructure/Symfony/Kernel.php
+++ src/Shared/Infrastructure/Symfony/Kernel.php
@@ -5,7 +5,7 @@ namespace Candice\Shared\Infrastructure\Symfony;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
-class Kernel extends BaseKernel
+final class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 }
