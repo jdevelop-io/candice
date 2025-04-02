db94860e2679abbc2f83c52f72be8d47f56a3aed
Jean-Denis VIDOT <5817251+jh3ady@users.noreply.github.com>
2025-04-02 22:37:41 +0200

    :art: chore(phpstan): Resolve problems

--- public/index.php
+++ public/index.php
@@ -4,6 +4,14 @@ use Candice\Shared\Infrastructure\Symfony\Kernel;
 
 require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool)$context['APP_DEBUG']);
+return function (array $context): Kernel {
+    if (!isset($context['APP_ENV']) || !is_string($context['APP_ENV'])) {
+        throw new RuntimeException('APP_ENV environment variable is not set or not valid');
+    }
+
+    if (!isset($context['APP_DEBUG']) || !is_bool($context['APP_DEBUG'])) {
+        throw new RuntimeException('APP_DEBUG environment variable is not set or not valid');
+    }
+
+    return new Kernel($context['APP_ENV'], $context['APP_DEBUG']);
 };
