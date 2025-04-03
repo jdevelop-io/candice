69ce84294015b42f37f9bb42a98a670e1a3df638
Jean-Denis VIDOT <5817251+jh3ady@users.noreply.github.com>
2025-04-03 14:50:03 +0200

    :building_construction: chore(docker): Add web service

---
+++ docker/web/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+WORKDIR="/var/www/html"
+
+# Install dependencies if vendor directory does not exist or is empty
+if [ ! -d "$WORKDIR/vendor" ] || [ -z "$(ls -A $WORKDIR/vendor)" ]; then
+    composer install --no-interaction --no-progress
+fi
+
+# Run the command passed to the docker run command
+exec "$@"
